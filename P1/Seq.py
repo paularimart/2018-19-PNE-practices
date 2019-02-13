73360ff911d47f03234340e95881e83b3bc75139
paularimart <antoinerimart@gmail.com>
2019-02-13 18:48:12 +0100

    Practice 1

--- P1/Seq.py
+++ P1/Seq.py
@@ -43,6 +43,37 @@ class Seq:
             else:
                 result_G += 1
 
-        print("A:",result_A, "T:",result_T, "C:",result_C, "G:",result_G)
+        x = ["A:",result_A, "T:",result_T, "C:",result_C, "G:",result_G]
+        return x
 
-    #def perc(base):
+    def perc(base):
+        if seq_len > 0:
+            per_A = round(100.0 * num_A / seq_len, 1)
+        else:
+            per_A = 0
+
+        print("Base T")
+        num_T = dict_bases['T']
+        print(" The number of Ts is:", num_T)
+        if seq_len > 0:
+            per_T = round(100.0 * num_T / seq_len, 1)
+        else:
+            per_T = 0
+
+        print("Base C")
+        num_C = dict_bases['C']
+        print(" The number of Ts is:", num_C)
+        if seq_len > 0:
+            per_C = round(100.0 * num_C / seq_len, 1)
+        else:
+            per_C = 0
+
+        print("Base G")
+        num_G = dict_bases['G']
+        print(" The number of Gs is:", num_G)
+        if seq_len > 0:
+            per_G = round(100.0 * num_G / seq_len, 1)
+        else:
+            per_G = 0
+
+        print("A: ", per_A, "T: ", per_T, "C: ", per_C, "G: ", per_G)
