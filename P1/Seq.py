9a7869550aa328fe4f319e8a33e118c4eb790224
Paula Arias <antoinerimart@gmail.com>
2019-02-18 20:58:41 +0100

    Practice 1 - Object Oriented Programming

--- P1/Seq.py
+++ P1/Seq.py
@@ -46,34 +46,43 @@ class Seq:
         x = ["A:",result_A, "T:",result_T, "C:",result_C, "G:",result_G]
         return x
 
-    def perc(base):
+    def perc(self):
+
+        seq_len = len(self.strbases)
+
+        result_A = 0
+        result_T = 0
+        result_C = 0
+        result_G = 0
+
+        for x in self.strbases:
+            if x == "A":
+                result_A += 1
+            elif x == "T":
+                result_T += 1
+            elif x == "C":
+                result_C += 1
+            else:
+                result_G += 1
+
         if seq_len > 0:
-            per_A = round(100.0 * num_A / seq_len, 1)
+            per_A = round(100.0 * result_A / seq_len, 1)
         else:
             per_A = 0
 
-        print("Base T")
-        num_T = dict_bases['T']
-        print(" The number of Ts is:", num_T)
         if seq_len > 0:
-            per_T = round(100.0 * num_T / seq_len, 1)
+            per_T = round(100.0 * result_T / seq_len, 1)
         else:
             per_T = 0
 
-        print("Base C")
-        num_C = dict_bases['C']
-        print(" The number of Ts is:", num_C)
         if seq_len > 0:
-            per_C = round(100.0 * num_C / seq_len, 1)
+            per_C = round(100.0 * result_C / seq_len, 1)
         else:
             per_C = 0
 
-        print("Base G")
-        num_G = dict_bases['G']
-        print(" The number of Gs is:", num_G)
         if seq_len > 0:
-            per_G = round(100.0 * num_G / seq_len, 1)
+            per_G = round(100.0 * result_G / seq_len, 1)
         else:
             per_G = 0
 
-        print("A: ", per_A, "T: ", per_T, "C: ", per_C, "G: ", per_G)
+        return ("A: ", per_A, "T: ", per_T, "C: ", per_C, "G: ", per_G)
