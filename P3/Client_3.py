6bf7fde28f7beea86976d09db60eab0a7e482172
paularimart <antoinerimart@gmail.com>
2019-02-19 16:57:53 +0100

    Practice 3

---
+++ P3/Client_3.py
@@ -0,0 +1,24 @@
+import socket
+
+PORT = 8081
+IP = "212.128.253.67"
+
+while True:
+    # The client is blocking the server....  NOT A GOOD DESIGN!!!
+    msg = input("> ")
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # establish the connection to the Server (IP, PORT)
+    s.connect((IP, PORT))
+
+    # Send the request message to the server
+    s.send(str.encode(msg))
+
+    # Receive the servers respoinse
+    response = s.recv(2048).decode()
+
+    # Print the server's response
+    print("Response: {}".format(response))
+
+    s.close()
---
+++ P3/Server_3.py
@@ -0,0 +1,64 @@
+import socket
+
+PORT = 8081
+IP = "212.128.253.67"
+#Maximum number of clients that can connect to this server
+MAX_OPEN_REQUEST = 5
+
+def check(s):
+    if s == "":
+        print("ALIVE")
+    else:
+        for x in s:
+            if x != "A" and x != "T" and x != "C" and x != "G":
+                print("ERROR")
+                break
+            else:
+                print("OK")
+
+def length(s):
+    return len(s)
+
+def process_client(cs):
+
+    #ECHO SERVER
+    #Reading the message from the client
+    msg = cs.recv(2048).decode("utf-8")
+
+    #print("Message from the client: {}". format(msg))
+    # Sending the message back to the client
+    #cs.send(str.encode(msg))
+
+    if msg == "len":
+        x = length(msg)
+        print("The length of the sequence is: {}". format(x))
+        cs.send(str.encode(msg))
+    else:
+        check(msg)
+
+
+    cs.close()
+
+#Create a socket for connecting to the clients
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serversocket.bind((IP, PORT))
+
+#To listen to the client's requests
+serversocket.listen(MAX_OPEN_REQUEST)
+
+print("Socket ready: {}". format(serversocket))
+
+while True:
+
+    print("Waiting for connections at: {}, {}". format(IP, PORT))
+    (clientsocket, adress) = serversocket.accept()
+
+    #-- Process the client request
+    print("Attending client: {}". format(adress))
+
+    process_client(clientsocket)
+
+
+    #-- Close the socket
+    clientsocket.close()
