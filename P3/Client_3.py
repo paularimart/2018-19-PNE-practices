4e86aa76b9ae67dc12539692115484aa1d2dbadb
paularimart <antoinerimart@gmail.com>
2019-02-27 18:51:08 +0100

    Practice 3

--- P3/Client_3.py
+++ P3/Client_3.py
@@ -1,27 +1,23 @@
 import socket
 
-PORT = 8087
-IP = "212.128.253.74"
+PORT = 8081
+IP = "212.128.253.67"
 
-while True:
-    # The client is blocking the server....  NOT A GOOD DESIGN!!!
-    msg = """AAACGTG 
-          len 
-          perc
-          """
+# The client is blocking the server....  NOT A GOOD DESIGN!!!
+msg = "AAACGTG\nlen"
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # establish the connection to the Server (IP, PORT)
-    s.connect((IP, PORT))
+# establish the connection to the Server (IP, PORT)
+s.connect((IP, PORT))
 
-    # Send the request message to the server
-    s.send(str.encode(msg))
+# Send the request message to the server
+s.send(str.encode(msg))
 
-    # Receive the servers respoinse
-    response = s.recv(2048).decode()
+# Receive the servers respoinse
+response = s.recv(2048).decode()
 
-    # Print the server's response
-    print("Response: {}".format(response))
+# Print the server's response
+print("Response: {}".format(response))
 
-    s.close()
+s.close()
--- P3/Server_3.py
+++ P3/Server_3.py
@@ -1,24 +1,21 @@
 import socket
 
-PORT = 8087
-IP = "212.128.253.74"
+PORT = 8081
+IP = "212.128.253.67"
 #Maximum number of clients that can connect to this server
 MAX_OPEN_REQUEST = 5
 
 def check(s):
-    if s == "":
-        print("ALIVE")
-    else:
-        for x in s:
-            if x != "A" and x != "T" and x != "C" and x != "G":
-                seq = "bad"
-                break
-            else:
-                seq = "good"
-        if seq == "bad":
-            print("ERROR")
+    for x in s:
+        if x != "A" and x != "T" and x != "C" and x != "G":
+            seq = "bad"
+            break
         else:
-            print("OK")
+            seq = "good"
+    if seq == "bad":
+        print("ERROR")
+    else:
+        print("OK")
 
 def length(s):
     return len(s)
@@ -29,16 +26,19 @@ def process_client(cs):
     #Reading the message from the client
     msg = cs.recv(2048).decode("utf-8")
 
-    #print("Message from the client: {}". format(msg))
-    # Sending the message back to the client
-    #cs.send(str.encode(msg))
+    message = msg.split("\n")
 
-    if msg == "len":
-        x = length(msg)
-        print("The length of the sequence is: {}". format(x))
-        cs.send(str.encode(msg))
+    print("Message from the client: {}". format(message))
+
+    if message[0] == "":
+        print("ALIVE")
     else:
-        check(msg)
+        check(message[0])
+        for x in message:
+            if x == "len":
+                x = length(message[0])
+                print("The length of the sequence is: {}".format(x))
+                cs.send(str.encode(msg))
 
     cs.close()
 
