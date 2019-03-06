c8d65c3302ad48a741d1a5212f5ca0b329aa5d27
paularimart <antoinerimart@gmail.com>
2019-03-06 17:49:57 +0100

    Session 13 - HTTP model

--- Session-13/server-ex2.py
+++ Session-13/server-ex2.py
@@ -1,7 +1,9 @@
 import http.server
 import socketserver
+import termcolor
 
-PORT = 8000
+PORT = 8080
+content = ''
 
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
@@ -9,24 +11,32 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         print("GET  received")
 
+        request_line = self.requestline
+
         print("Request line:" + self.requestline)
         print("  Cmd: " + self.command)
         print("  Path: " + self.path)
 
-        content = "I am the happy server! :-)"
+        if request_line.startswith("GET / ") or request_line.startswith("GET /index"):
+            file = open("index-ex2.html", "r")
+            content = file.read()
+        else:
+            file = open("error-ex2.html", "r")
+            content = file.read()
 
         self.send_response(200)
-        self.send_header('Content-Type', 'text/plain')
-        self.send_header('COntent-Length', len(str.encode(content)))
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(content)))
         self.end_headers()
 
         self.wfile.write(str.encode(content))
 
         return
 
+
 Handler = TestHandler
 
-with socketserver.TCPServer(("212.128.253.67", PORT), Handler) as httpd:
+with socketserver.TCPServer(("212.128.253.68", PORT), Handler) as httpd:
     print("Serving at PORT", PORT)
 
     httpd.serve_forever()
