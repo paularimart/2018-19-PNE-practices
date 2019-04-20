29ecc8a5220f0cecc1b2ead1a54e30a79972d2a2
Paula Arias <antoinerimart@gmail.com>
2019-04-20 14:31:39 +0200

    practice6

--- P6/WebServer.py
+++ P6/WebServer.py
@@ -29,10 +29,42 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                     #file = open("Error.html", "r")
                     #contents = file.read()
 
-            if functions[1] == 'chk=on':
+            if (functions[1] == 'chk=on')and(functions[2] == 'base=A') or (functions[2] == 'base=T') or (functions[2] == 'base=C') or (functions[2] == 'base=G') and (functions[3] == 'operation=count'):
                 seq = functions[0].split('=')
                 length = len(seq[1])
-                print(length)
+                if functions[2] == 'base=A':
+                    seq = functions[0].split('=')
+                    result_A = 0
+                    base = 'A'
+                    for x in seq[1]:
+                        if x == "A":
+                            result_A += 1
+                    count = result_A
+                elif functions[2] == 'base=T':
+                    base = 'T'
+                    seq = functions[0].split('=')
+                    result_T = 0
+                    for x in seq[1]:
+                        if x == "T":
+                            result_T += 1
+                    count = result_T
+                elif functions[2] == 'base=C':
+                    seq = functions[0].split('=')
+                    result_C = 0
+                    base = 'C'
+                    for x in seq[1]:
+                        if x == "C":
+                            result_C += 1
+                    count = result_C
+                elif functions[2] == 'base=G':
+                    seq = functions[0].split('=')
+                    result_G = 0
+                    base = 'G'
+                    for x in seq[1]:
+                        if x == "G":
+                            result_G += 1
+                    count = result_G
+
                 contents = """<!DOCTYPE html>
                                  <html lang="en">
                                    <head>
@@ -43,10 +75,14 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                                      <h1>Response</h1>
                                      Length:
                                      <l>{}</l>
+                                     <br>
+                                     Count of {} bases:
+                                     <l>{}</l>
                                      <br><br>
                                      <a href="/">HOME PAGE</a>
                                    </body>
-                                 </html>""".format(length)
+                                 </html>""".format(length, base, count)
+
 
         else:
             file = open("Error.html", "r")
