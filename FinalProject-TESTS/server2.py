56bb512bf099362e6e967cfa1240be162c0de26c
paularimart <antoinerimart@gmail.com>
2019-04-24 17:40:13 +0200

    tryouts final practice

--- FinalProject-TESTS/server2.py
+++ FinalProject-TESTS/server2.py
@@ -21,55 +21,95 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             contents = f.read()
 
         elif resource == '/listSpecies':
-            #print(self.path)
-            #resource_2 = self.path.split('?')
-            #print(resource_2)
-            #options = resource_2[1]
-            #print(options)
-            #param_list = options.split('&')
-            #print(param_list)
+            if len(path_list) == 1:
+                HOSTNAME = 'rest.ensembl.org'
+                ENDPOINT_LIST_SPECIES = '/info/species?content-type=application/json'
+                METHOD = "GET"
 
-            #if param_list[0] == 'list_species=on':
-            HOSTNAME = 'rest.ensembl.org'
-            ENDPOINT_LIST_SPECIES = '/info/species?content-type=application/json'
-            METHOD = "GET"
+                headers = {'User-Agent': 'http-client'}
 
-            headers = {'User-Agent': 'http-client'}
+                conn = http.client.HTTPConnection(HOSTNAME)
 
-            conn = http.client.HTTPConnection(HOSTNAME)
+                conn.request(METHOD, ENDPOINT_LIST_SPECIES, None, headers)
 
-            conn.request(METHOD, ENDPOINT_LIST_SPECIES, None, headers)
+                r1 = conn.getresponse()
 
-            r1 = conn.getresponse()
+                print()
+                print("Response received: ", end='')
+                print(r1.status, r1.reason)
+                print()
 
-            print()
-            print("Response received: ", end='')
-            print(r1.status, r1.reason)
-            print()
+                text_json = r1.read().decode("utf-8")
+                conn.close()
 
-            text_json = r1.read().decode("utf-8")
-            conn.close()
+                species = json.loads(text_json)
 
-            species = json.loads(text_json)
+                list_species = ''
+                for x in species['species']:
+                    list = x['name']
+                    print(list)
+                    list_species = list_species + '<li>{}</li>'.format(list)
+                    contents = """<!DOCTYPE html>
+                                                 <html lang="en">
+                                                   <head>
+                                                     <meta charset="utf-8">
+                                                     <title>List Species</title>
+                                                   </head>
+                                                   <body style="background-color: #7cc3f3;">
+                                                     <h1>List of species</h1>
+                                                     <l>{}</l>
+                                                     <br><br>
+                                                     <a href="/">HOME PAGE</a>
+                                                   </body>
+                                                 </html>""".format(list_species)
+            elif len(path_list) > 1:
+                request = path_list[1].split('=')
+                limit = request[1]
+                print(limit)
 
-            list_species = ''
-            for x in species['species']:
-                list = x['name']
-                print(list)
-                list_species = list_species + '<li>{}</li>'.format(list)
-                contents = """<!DOCTYPE html>
-                                 <html lang="en">
-                                   <head>
-                                     <meta charset="utf-8">
-                                     <title>List Species</title>
-                                   </head>
-                                   <body style="background-color: #7cc3f3;">
-                                     <h1>List of species</h1>
-                                     <l>{}</l>
-                                     <br><br>
-                                     <a href="/">HOME PAGE</a>
-                                   </body>
-                                 </html>""".format(list_species)
+                HOSTNAME = 'rest.ensembl.org'
+                ENDPOINT_LIST_SPECIES = '/info/species?content-type=application/json'
+                METHOD = "GET"
+
+                headers = {'User-Agent': 'http-client'}
+
+                conn = http.client.HTTPConnection(HOSTNAME)
+
+                conn.request(METHOD, ENDPOINT_LIST_SPECIES, None, headers)
+
+                r1 = conn.getresponse()
+
+                print()
+                print("Response received: ", end='')
+                print(r1.status, r1.reason)
+                print()
+
+                text_json = r1.read().decode("utf-8")
+                conn.close()
+
+                species = json.loads(text_json)
+
+                list_species = ''
+                y = 0
+                for x in species['species']:
+                    y += 1
+                    if y <= int(limit):
+                        list = x['name']
+                        print(list)
+                        list_species = list_species + '<li>{}</li>'.format(list)
+                        contents = """<!DOCTYPE html>
+                                         <html lang="en">
+                                           <head>
+                                             <meta charset="utf-8">
+                                             <title>List Species</title>
+                                           </head>
+                                           <body style="background-color: #7cc3f3;">
+                                             <h1>List of species</h1>
+                                             <l>{}</l>
+                                             <br><br>
+                                             <a href="/">HOME PAGE</a>
+                                           </body>
+                                         </html>""".format(list_species)
 
         elif resource == '/karyotype':
         #elif param_list[1] == 'karyotype=on':
@@ -134,6 +174,9 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             specie_k = param_list[0].split('=')
             animal = specie_k[1]
             print(animal)
+            chromo = param_list[1].split('=')
+            num_chromo = chromo[1]
+            print(num_chromo)
             METHOD = "GET"
 
             headers = {'User-Agent': 'http-client'}
@@ -149,15 +192,13 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
             chromosomes = json.loads(text_json)
 
-            chromosome = ''
             for x in chromosomes['karyotype']:
                 step1 = chromosomes['top_level_region']
                 for y in step1:
                     step2 = y['name']
-                    if step2 == x:
+                    if step2 == num_chromo:
                         step3 = y['length']
-                        print(x, step3)
-                        chromosome = chromosome + '<li>{}: {}</li>'.format(x, step3)
+                        print(num_chromo, step3)
                         contents = """<!DOCTYPE html>
                                          <html lang="en">
                                            <head>
@@ -166,12 +207,11 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                                            </head>
                                            <body style="background-color: #7cc3f3;">
                                              <h1>Chromosome length</h1>
-                                             <l>{}</l>
+                                             <l>{}: {}</l>
                                              <br><br>
                                              <a href="/">HOME PAGE</a>
                                            </body>
-                                         </html>""".format(chromosome)
-
+                                         </html>""".format(num_chromo, step3)
 
         else:
             file = open("error1.html", "r")
